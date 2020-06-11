8154e8c51338f0786e4a1dda57662cf68b50c5f9
H.Lee <45928371+hyeongilee@users.noreply.github.com>
2020-06-11 14:31:40 +0900

    Update event2image.py

--- event2image.py
+++ event2image.py
@@ -12,38 +12,24 @@ class Struct:
 def main(argv):
     inputfile = ''
     outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print('aedat4tomat.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
+    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     for opt, arg in opts:
-        if opt == '-h':
-            print('aedat4tomat.py -i <inputfile> -o <outputfile>')
-            sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print('Input file is "', inputfile)
-    print('Output file is "', outputfile)
-    
+            outputfile = arg    
 
     data = {'aedat': out}
 
     with AedatFile(inputfile) as f:
-
-        # loop through the "events" stream
+        
         fps = 30.0
         dt = 1/(fps/1000.0)
-        #initial = f['events']
         I = np.zeros((240,320))
         prev_time = 0
-        initial_time = 1588242864305911
+        initial_time = 1588242864305911 #modify the value
         for e in f['events']:
-            #print(e.timestamp)
             time = (e.timestamp - initial_time)//1000
-            #print(time)
             time = int(time//dt)
             x = e.x
             y = e.y
@@ -53,10 +39,7 @@ def main(argv):
             I[y][x] = 255.0
             prev_time = time
             
-        
-    #Add counts
     out.data.polarity.numEvents = len(out.data.polarity.x)
-    #out.data.imu6.numEvents = len(out.data.imu6.accelX)
     sio.savemat(outputfile, data)
     
 if __name__ == "__main__":
